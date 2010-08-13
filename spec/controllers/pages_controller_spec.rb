19d8e578a64ec632f83bdfc2183a0215a599a938
davidallwhole <magcig@fastwebnet.it>
2010-08-13 21:49:01 +0200

    Add .autotest

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,12 +2,6 @@ require 'spec_helper'
 
 describe PagesController do
 
-  #Delete these examples and add some real ones
-  it "should use PagesController" do
-    controller.should be_an_instance_of(PagesController)
-  end
-
-
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -20,5 +14,11 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+  end
+  
+  describe "GET 'contact'" do
+    it "should be sucessful" do
+      get 'about'
+      response.should be_success
   end
 end
